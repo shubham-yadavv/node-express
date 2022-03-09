9c333be49bd24c8061a50b86d5ae58faa6698f09
shubham <shubham22121@gmail.com>
2022-03-09 23:08:30 +0530

    express

--- 02-express/01-http-basics.js
+++ 02-express/01-http-basics.js
@@ -2,7 +2,7 @@ const http = require('http')
 
 const hostname = 'localhost';
 const port = 3000;
-
+z
 const server = http.createServer((req, res) => {
   // console.log(req.method)
   const url = req.url
